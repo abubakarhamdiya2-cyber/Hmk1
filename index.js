ef0ab71f343e2916ce297c339ac4b4e7bd74c235
abubakarhamdiya2-cyber <abubakarhamdiya2@gmail.com>
2026-07-20 13:40:14 +0000

    update script.js

---
+++ index.js
@@ -0,0 +1,2 @@
+// Taking input from a user and storing it in a variable called userName
+prompt("index.js")
---
+++ src/script.js
@@ -0,0 +1,9 @@
+alert(" welcome to our page");
+
+let name=prompt("input Name");
+
+let age=prompt("input age");
+
+let email=prompt("input email");
+
+let message=alert(`hello ${name},you are ${ age}, years old we'll keep in touch via your email ${email}`);
