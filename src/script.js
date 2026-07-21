7077f11e2fd6c8297cde154623222cd1c5f92dd3
abubakarhamdiya2-cyber <abubakarhamdiya2@gmail.com>
2026-07-21 16:04:59 +0000

    update all files

--- src/script.js
+++ src/script.js
@@ -7,3 +7,52 @@ let age=prompt("input age");
 let email=prompt("input email");
 
 let message=alert(`hello ${name},you are ${ age}, years old we'll keep in touch via your email ${email}`);
+
+const mystring = "HAMDIYA";
+let myname = "HAMDIYA";
+
+let myage = "28"; 
+
+console.log(myname);
+myname = "HAMDIYA";
+
+ console.log(myage);
+ myage = "28";
+
+ let x = 15
+ let y = 10
+
+ let addnumbers = x + y
+ console.log (addnumbers); // output = 25
+ console.log (x+y); // output = 25
+
+ let subtraction = x - y
+ console.log (subtraction );// output =5
+ console.log (x - y)// output = 5
+
+ let divide = x/y
+ console.log (divide );// output =1.5
+ console.log (x/y)// output = 1.5
+
+let multiply = x * y
+ console.log (multiply );// output =150
+ console.log (x * y)// output = 150
+
+ let remainder = y % x; // output 
+console.log(remainder)
+ 
+x = 15;
+y = 4;
+
+remainder = y % x;
+console.log( remainder); // 2
+addNumbers = x + y;
+productNumbers = x * y;
+divideNumbers = y/x;
+remainder = y % x;
+
+console.log (`today we learned how to add, subtruct, divide, given that x is ${x} and y is ${y} the sum will be ${addNumbers}
+    the difference is ${x-y}, the product will be ${productNumbers}, the qoutient will be${divideNumbers}, and the
+    reminder will be ${remainder}`)
+
+
